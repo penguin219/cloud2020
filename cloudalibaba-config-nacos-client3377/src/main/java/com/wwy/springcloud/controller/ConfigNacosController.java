ebed76a01a97ce5d9aa9516530510d0a298268ca
penguin219 <wwy186p@163.com>
2020-04-25 16:52:01 +0800

    添加nacos模块，作为服务配置中心

---
+++ cloudalibaba-config-nacos-client3377/src/main/java/com/wwy/springcloud/controller/ConfigNacosController.java
@@ -0,0 +1,19 @@
+package com.wwy.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope //通过springcloud的原生注解实现配置自动更新
+public class ConfigNacosController {
+
+    @Value("${config.info}")
+    private String configInfo;
+
+    @GetMapping("/config/info")
+    public String getConfigInfo(){
+        return configInfo;
+    }
+}
